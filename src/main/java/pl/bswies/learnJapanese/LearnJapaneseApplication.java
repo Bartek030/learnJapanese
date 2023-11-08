580c435215c78f07e5acb6ea73409299a1bef5bb
Bartlomiej Swies <bartlomiejswies@gmail.com>
2023-11-09 00:01:54 +0100

    Basic app configuration

---
+++ src/main/java/pl/bswies/learnJapanese/LearnJapaneseApplication.java
@@ -0,0 +1,13 @@
+package pl.bswies.learnJapanese;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJapaneseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJapaneseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/bswies/learnJapanese/LearnJapaneseApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bswies.learnJapanese;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJapaneseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
